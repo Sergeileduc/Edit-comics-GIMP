8fa428c489890981a1b01898baaeea54553a7820
Sergeileduc <sergei.leduc@gmail.com>
2018-07-03 13:41:55 +0200

    clean-bulle init commit

---
+++ scripts/script-clean-bubble.scm
@@ -0,0 +1,36 @@
+(define (script-fu-clean-bubble image drawable)
+	
+	;variable
+	(let* (
+		(drawable (car (gimp-image-active-drawable image))))
+	)
+
+	;Prep
+	(gimp-context-push)
+	(gimp-image-undo-group-start image)
+
+	; Enlève les trous (le lettrage VO) de la sélection
+	(gimp-selection-flood image)
+	
+	;remplit la sélection avec la couleur de PP
+	;(gimp-drawable-edit-fill drawable FILL-FOREGROUND)
+	(gimp-drawable-edit-fill (car(gimp-image-get-active-drawable image)) FILL-BACKGROUND)
+
+	;Finish
+	(gimp-displays-flush)
+	(gimp-selection-none image)
+	(gimp-image-undo-group-end image)
+	(gimp-context-pop)
+	)
+
+(script-fu-register "script-fu-clean-bubble"
+	"<Image>/DCT-trad/1) Clean bulle..."
+	"Clean la/les bulle(s) selectionnées avec la couleur AP
+	Sélectionnez vos bulles en les sélectionnant avec la baguette magique avant de lancer le script"
+	"Sergeileduc"
+	"Sergeileduc"
+	"2008-06-28"
+	"RGB*"
+	SF-IMAGE "Input Image" 0
+	SF-DRAWABLE "Current Layer" 0
+)
