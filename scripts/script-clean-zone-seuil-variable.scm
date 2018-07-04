17c7b76802021330b63c7de1cb8ce8bc807a3b1b
Sergeileduc <sergei.leduc@gmail.com>
2018-07-04 13:24:37 +0200

    clean zone seuil -> drawable

--- scripts/script-clean-zone-seuil-variable.scm
+++ scripts/script-clean-zone-seuil-variable.scm
@@ -2,8 +2,8 @@
 
 	;variable
 	(let* (
-		(drawable (car (gimp-image-active-drawable image)))
-		)
+	(drawable (car (gimp-image-active-drawable image)))
+	)
 
 	;Prep
 	(gimp-context-push)
@@ -13,9 +13,9 @@
 	;script
 	(gimp-image-get-selection image)
 	;(gimp-image-select-color image 3 drawable (car (gimp-context-get-background)))
-	(gimp-image-select-color image 3 (car (gimp-image-get-active-drawable image)) (car (gimp-context-get-background)))
+	(gimp-image-select-color image 3 drawable (car (gimp-context-get-background)))
 	(gimp-selection-flood image)
-	(gimp-drawable-edit-fill (car (gimp-image-get-active-drawable image)) FILL-BACKGROUND)
+	(gimp-drawable-edit-fill drawable FILL-BACKGROUND)
 
 	;Finish
 	(gimp-displays-flush)
@@ -23,7 +23,7 @@
 	(gimp-image-undo-group-end image)
 	(gimp-context-pop)
 
-);end let
+	);end let
 )
 
 (script-fu-register "script-fu-clean-zone-seuil-variable"
