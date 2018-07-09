c5ca227954a7170aa2913ae945a0c1208a16ab3a
Sergeileduc <sergei.leduc@gmail.com>
2018-07-09 21:20:49 +0200

    Ajout d'un seuil de détection pour les lettres

--- scripts/script-clean-zone-texture.scm
+++ scripts/script-clean-zone-texture.scm
@@ -1,8 +1,9 @@
-(define (script-fu-clean-texture image drawable grow-pixel sampling-width sample-from filling-order flou)
+(define (script-fu-clean-texture image drawable seuil grow-pixel sampling-width sample-from filling-order flou)
 
 	;Prep
 	(gimp-context-push)
 	(gimp-image-undo-group-start image)
+	(gimp-context-set-sample-threshold-int seuil)
 
 	(let* (
 		(drawable (car (gimp-image-active-drawable image)))
@@ -19,7 +20,13 @@
 	(gimp-image-get-selection image)
 	(gimp-image-select-color image CHANNEL-OP-INTERSECT drawable (car (gimp-context-get-foreground)))
 	(gimp-selection-grow image grow-pixel)
-	(python-fu-heal-selection 1 image drawable sampling-width sample-from filling-order)
+	(if (= (car (gimp-selection-is-empty image)) FALSE)
+		(python-fu-heal-selection 1 image drawable sampling-width sample-from filling-order)
+		(gimp-message "Aucune sélection !\
+Veuillez sélectionner la zone à corriger\
+La couleur de Premier Plan doit être de la couleur des LETTRES\
+Utilisez la PIPETTE")
+	)
 
 	;flou
 	(gimp-image-select-round-rectangle image 2 select-x1 select-y1 select-width select-height 5 5)
@@ -35,25 +42,27 @@
 
 (script-fu-register "script-fu-clean-texture"
 	"2) Clean zone texturée ou transparente..."
-	"Il faut que la couleur de PP soit la couleur des lettres
-	\(Utilisez la PIPETTE -> le noir des lettres n'est jamais du  vrai \(0 0 0\),
-	souvent c'est du gris foncé \(30 30 30\)\),
-	alors utilisez la pipette sur les lettres..
-
-	Veuillez d'abord sélectionner une zone \(avec l'outil de sélection RECTANGULAIRE par exemple\)"
+	"Il faut que la couleur de PP soit la couleur des lettres\
+\(Utilisez la PIPETTE -> le noir des lettres n'est jamais du  vrai \(0 0 0\),\
+souvent c'est du gris foncé \(30 30 30\)\),
+\alors utilisez la pipette sur les lettres..
+......................\
+Veuillez d'abord sélectionner une zone \(avec l'outil de sélection RECTANGULAIRE par exemple\)"
 	"Sergeileduc"
 	"Sergeileduc"
 	"2018-06-28"
 	"RGB*"
 	SF-IMAGE "Input Image" 0
 	SF-DRAWABLE "Current Layer" 0
-	SF-ADJUSTMENT "Agrandir la sélection autour des lettres de n (pixels)
-	\(1 ou 2, agrandir si nécéssaire, si la bordure des lettres apparaît toujours après correction" '(2 0 10 1 10 0 0)
+	SF-ADJUSTMENT "Seuil de tolérance à la couleur des LETTRES (défault = 30)" '(30 10 70 1 10 0 0)
+	SF-ADJUSTMENT "Agrandir la sélection autour des lettres de n (pixels)\
+\1 ou 2 généralement, jouer plutôt sur le seuil si les lettres disparaissent mal" '(1 0 10 1 10 0 0)
 	SF-ADJUSTMENT "Context sampling width \(pixels\)" '(10 0 100 1 10 0 0)
 	SF-OPTION "Sample from" '("All around" "Sides" "Above and below")
 	SF-OPTION "Filling order" '("Random" "Inwards towards center" "Outwards from center")
 	;SF-TOGGLE "Flouter avec un flou gaussien après la correction ?" FALSE
-	SF-ADJUSTMENT "Intensité du flou gaussien à appliquer en fin de script pour gommer les défauts" '(0 0 10 1 10 0 0)
+	SF-ADJUSTMENT "Intensité du flou gaussien à appliquer en fin de script pour gommer les défauts\
+0 pas de flou / 2 léger flou" '(0 0 10 1 10 0 0)
 )
 
 ( script-fu-menu-register
